b9c9c37c9caa2b666184ae0c1ba08f9b94683a9a
Mateusz Majka <47884779+Reckit075@users.noreply.github.com>
2022-06-21 14:59:32 +0200

    useEffect, useMemo learn some new things about two popular hooks, (render twice in new version of react, importanant notice)

--- src/topics/newHooks/index.tsx
+++ src/topics/newHooks/index.tsx
@@ -1,6 +1,33 @@
-import { useId } from "react";
+import { useState, useId, useMemo, useEffect } from "react";
 
 export default function TestHooks() {
+  const [trigger, setTrigger] = useState(true);
+
+  useEffect(() => {
+    console.log("useEffect working now!");
+  }, [trigger]);
+  console.log("test");
+
   const id = useId();
-  return <p id={id}>testHooks</p>;
+
+  const expensiveOperation = (a: number, b: number) => {
+    for (let i = 0; i < 100; i++) {
+      a += a + b;
+      b += a + b;
+    }
+    return [a, b];
+  };
+  let a: number = 1;
+  let b: number = 1;
+  const memoizedValue = useMemo(() => expensiveOperation(a, b), [a, b]);
+
+  return (
+    <>
+      <p id={id}>testHooks</p>
+      <hr />
+      <span>{memoizedValue}</span>
+      <hr />
+      <button onClick={() => setTrigger(!trigger)}>run useEffect</button>
+    </>
+  );
 }
